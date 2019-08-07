e76fad4e4ea21e84ab53c722349fc470a13a751e
Jorge Vargas <jorgev259@outlook.es>
2019-08-07 16:33:42 -0600

    separate debug

--- main_debug.js
+++ main_debug.js
@@ -8,6 +8,7 @@ const MongoStore = require('connect-mongo')(session)
 
 var express = require('express')
 var app = express()
+var router = express.Router()
 
 var http = require('http').createServer(app)
 var io = require('socket.io')(http)
@@ -25,16 +26,15 @@ async function startup () {
   app.set('views', path.join(__dirname, '/public'))
   app.set('port', process.env.PORT || 3005)
 
-  app.use(require('./routes/acceso')(io))
-  app.use(require('./routes/extractora')(io))
-  app.use(require('./routes/tunel')(io))
+  router.use(require('./routes/acceso')(io))
+  router.use(require('./routes/extractora')(io))
+  router.use(require('./routes/tunel')(io))
 
-  if (argv.d) {
-    app.use('/js', express.static(path.join(__dirname, '/public/js')))
-    app.use('/img', express.static(path.join(__dirname, '/public/img')))
-    app.use('/style', express.static(path.join(__dirname, '/public/style')))
-    app.use('/favicon.ico', express.static(path.join(__dirname, '/public/favicon.ico')))
-  }
+  router.use('/js', express.static(path.join(__dirname, '/public/js')))
+  router.use('/img', express.static(path.join(__dirname, '/public/img')))
+  router.use('/style', express.static(path.join(__dirname, '/public/style')))
+  router.use('/favicon.ico', express.static(path.join(__dirname, '/public/favicon.ico')))
+  app.use('/vnode', router)
 
   http.listen(app.get('port'), function () {
     console.log('Web server listening on port ' + app.get('port'))
